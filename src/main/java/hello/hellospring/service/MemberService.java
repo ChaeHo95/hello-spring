d303f1e0ea7f9dc73eadb31bfa688b7e41b89d9a
ChaeHo95 <chlcogh11@naver.com>
2022-06-14 00:22:25 +0900

    회원 서비스 개발 1. Optional<>을 이용한 코드 작성 시 예전에 비해 조금 더 편한 코드 작성이 가능 하며, 코드가 간결 해짐 2. 메소드 안에 로직이 나올 때는 메소드로 뽑아 그 클래스안에서만 쓸수 있는 메소드로 전환하는 것이 좋은 습관

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,59 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+/*
+* repository는 단순히 기술 의존적으로 네이밍 하지만 service쪽은 비지니스 의존적으로 네이밍*/
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    /*
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+
+        // 로직이 나올 때는 메소드로 뽑는게 좋기 때문에 메소드로 전환
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 중복 회원 검증
+
+        // Optional<> 바로 반환하는 것은 권장하지 않아
+        // 반환 값이 Optional<> 이기때문에 이런 식으로 코드 작성 사용
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        /*Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> { //ifPresent() 메소드는 만약 값이 있으면 로직이 동작함
+            // 과거에는 if를 이용하여 하였지만 현재는 Optional<> 클래스의 ifPresent() 메소드를  많이 사용함.
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });*/
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /*
+    * 특정 회원 조회
+    * */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
